38181ed40ebd1eb4076cc4a576082309bb9e4cf5
Alessio Mastrucci <mastrucc@iiasa.ac.at>
2023-08-16 17:55:14 +0200

    change path definition in main.R
    change path definitions to be independent of current working directory

--- main.R
+++ main.R
@@ -9,59 +9,44 @@ library(parallel)
 suppressPackageStartupMessages(library(tidyverse))
 
 options(dplyr.width = Inf)
-options(dplyr.summarise.inform = FALSE)
 
 # rounding (number of decimals)#
-start_script_time <- Sys.time()
 rnd <- 5
 u_EJ_GWa <- 31.71
 
-source("./STURM_model/F10_scenario_run.R")
 
-# paths
-path_rcode <- paste(getwd(), "/STURM_model/", sep = "")
-path_in <- paste(getwd(), "/STURM_data/", sep = "")
-path_out <- paste(getwd(), "/STURM_output/results/", sep = "")
+# Paths
+path_r <- paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/") # Path to folder containing the current R file
+rcode_path <- paste0(path_r, "/STURM_model/")
+data_path <- paste0(path_r, "/STURM_data/")
+# Lucas: Should we add date to output folder?
+rout_path <- paste0(path_r, "/STURM_output/")
+
+# Source STURM code
+source(paste0(rcode_path,"/F10_scenario_runs_MESSAGE_2100.R"))
 
 # configuration file STURM
 base_year <- 2015
 end_year <- 2050
 step_year <- 5
-runs <- c(
-    "EU_medium_heat",
-    "EU_medium_shell"
-    )
-runs <- c(
-    "EU_no",
-    "EU_double",
-    "EU_triple")
-
-runs <- c("EU",
-"EU_price_constant",
-"EU_price_2020_600")
-
-runs <- c("EU",
-"EU_emission_constant",
-"EU_emission_2020_400")
-
-runs <- "EU"
+runs <- c("EU_implementation",
+          "EU_implementation_nomfhq1rent",
+          "EU_implementation_double",
+          "EU_implementation_triple")
 
 region <- c("WEU", "EEU")
 sector <- "resid"
 file_inputs <- "input_list_resid_EU.csv"
+path_prices_message <- paste0(data_path,
+                              "input_csv/input_price/input_prices_R12.csv")
+path_prices <- paste0(data_path,
+                      "input_csv/input_price/input_prices_EU.csv")
+
 file_scenarios <- "scenarios_EU.csv"
-en_method <- "TABULA" # "VDD", "TABULA"
 
-runs <- c(
-    "EU",
-    "EU_no",
-    "EU_double",
-    "EU_triple")
-
-runs <- c("EU")
-
-energy_efficiency <- "exogenous"
+mod_arch <- "stock"
 # energy_efficiency <- "endogenous"
+energy_efficiency <- "exogenous"
 report <- list(var = c("energy"),
                type = c("STURM"),
                geo_level = "region_bld")
@@ -69,30 +54,22 @@ report <- list(var = c("energy"),
 yrs <- seq(base_year, end_year, step_year)
 
 for (run in runs) {
-
-    if (energy_efficiency == "exogenous") {
-        run <- paste(run, energy_efficiency, sep = "_")
-    }
-
-    sturm_scenarios <- run_scenario(
-        run = run,
-        scenario_name = run,
-        sector = sector,
-        path_in = path_in,
-        path_rcode = path_rcode,
-        path_out = path_out,
-        file_inputs = file_inputs,
-        file_scenarios = file_scenarios,
-        geo_level_report = report$geo_level,
-        yrs = yrs,
-        report_var = report$var,
-        report_type = report$type,
-        region = region,
-        energy_efficiency = energy_efficiency,
-        en_method = en_method
-    )
-
-end_script_time <- Sys.time()
-print(paste("Time to run script:",
-    round(end_script_time - start_script_time, 0), "seconds."))
+  sturm_scenarios <- run_scenario(
+    run = run,
+    scenario_name = run,
+    sector = sector,
+    path_in = data_path,
+    path_rcode = rcode_path,
+    path_out = rout_path,
+    path_prices = path_prices,
+    path_prices_message = path_prices_message,
+    file_inputs = file_inputs,
+    file_scenarios = file_scenarios,
+    geo_level_report = report$geo_level,
+    yrs = yrs,
+    report_var = report$var,
+    report_type = report$type,
+    region = region,
+    energy_efficiency = energy_efficiency
+  )
 }

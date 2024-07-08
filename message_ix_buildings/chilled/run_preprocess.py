6880f02ae7933cccb235301dc5f27273bd9d4072
Measrainsey Meng <measrainsey@pm.me>
2024-07-08 16:22:29 +0200

    Change to relative import paths

--- message_ix_buildings/chilled/run_preprocess.py
+++ message_ix_buildings/chilled/run_preprocess.py
@@ -1,7 +1,5 @@
-from message_ix_buildings.chilled.config import Config
-from message_ix_buildings.chilled.preprocess.message_raster import (
-    create_archetype_template_map,
-)
+from .config import Config
+from .preprocess.message_raster import create_archetype_template_map
 
 cfg = Config()
 

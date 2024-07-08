ad6d921326ad91e1757ae3163a2664bc9af58f54
Measrainsey Meng <measrainsey@pm.me>
2024-07-08 17:04:06 +0200

    Add check for .nc file

--- message_ix_buildings/chilled/run_preprocess.py
+++ message_ix_buildings/chilled/run_preprocess.py
@@ -1,6 +1,16 @@
-from preprocess.message_raster import create_archetype_template_map
-from utils.config import Config
+import os
+
+from preprocess.message_raster import create_archetype_template_map  # type: ignore
+from utils.config import Config  # type: ignore
+from utils.config import Config  # type: ignore
 
 cfg = Config()
 
-create_archetype_template_map(cfg)
+# check to see if "map_reg_MESSAGE_{config.node}.nc" exists
+# if not, create the file
+if not os.path.exists(
+    os.path.join(cfg.dle_path, "out", "raster", "map_reg_MESSAGE_{cfg.node}.nc")
+):
+    create_archetype_template_map(cfg)
+else:
+    print(f"map_reg_MESSAGE_{cfg.node}.nc already exists")

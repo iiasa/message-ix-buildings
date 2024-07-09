fe9d01978495776a2bac5c26c8f7e689d1a07f14
Measrainsey Meng <measrainsey@pm.me>
2024-07-09 11:14:56 +0200

    Create function to set arch inputs and regions files

--- message_ix_buildings/chilled/preprocess/archetypes.py
+++ message_ix_buildings/chilled/preprocess/archetypes.py
@@ -13,6 +13,65 @@ from utils.config import Config  # type: ignore
 from utils.util import get_project_root  # type: ignore
 
 
+def read_arch_inputs_df(config: "Config", suff: str):
+    root_path = get_project_root()
+    version_path = os.path.join(root_path, "data", "chilled", "version", config.vstr)
+
+    if config.arch_setting == "fixed":
+        input_file = os.path.join(version_path, "arch_input.xlsx")
+
+        if os.path.exists(input_file):
+            arch_inputs = pd.read_excel(input_file, sheet_name=suff, index_col="id")
+
+            return arch_inputs
+        else:
+            print(
+                "Archetypes input file "
+                + input_file
+                + " does not exist! Please create file for input."
+            )
+    elif config.arch_setting == "regional":
+        input_file = os.path.join(
+            version_path,
+            "arch_input_reg.xlsx",
+        )
+
+        if os.path.exists(input_file):
+            arch_inputs = pd.read_excel(input_file, sheet_name="arch")
+
+            return arch_inputs
+        else:
+            print(
+                "Archetypes input file "
+                + input_file
+                + " does not exist! Please create file for input."
+            )
+
+
+def read_arch_reg_df(config: "Config", arch: str):
+    root_path = get_project_root()
+    version_path = os.path.join(root_path, "data", "chilled", "version", config.vstr)
+
+    if config.arch_setting == "regional":
+        reg_file = os.path.join(
+            version_path,
+            "arch_regions.xlsx",
+        )
+
+        if os.path.exists(reg_file):
+            arch_reg = pd.read_excel(reg_file, sheet_name=arch)
+            return arch_reg
+        else:
+            print(
+                "Regional archetypes input file "
+                + reg_file
+                + " does not exist! Please create file for input."
+            )
+
+    else:
+        print("Archetypes are not regional. No regional file to read.")
+
+
 def create_archetypes(config: "Config"):
     root_path = get_project_root()
     version_path = os.path.join(root_path, "data", "chilled", "version", config.vstr)
@@ -38,93 +97,53 @@ def create_archetypes(config: "Config"):
     )
 
     for arch in config.archs:
-        suff = arch  # suffix
+        arch_inputs = read_arch_inputs_df(config, arch)
+        arch_reg = read_arch_reg_df(config, arch)
 
-        if config.arch_setting == "fixed":
-            input_file = os.path.join(version_path, "arch_input.xlsx")
-
-            if os.path.exists(input_file):
-                arch_inputs = pd.read_excel(input_file, sheet_name=suff, index_col="id")
-            else:
-                print(
-                    "Archetypes input file "
-                    + input_file
-                    + " does not exist! Please create file for input."
-                )
-
-        elif config.arch_setting == "regional":
-            input_file = os.path.join(
-                version_path,
-                "arch_input_reg.xlsx",
-            )
-
-            reg_file = os.path.join(
-                version_path,
-                "arch_regions.xlsx",
-            )
-
-            if os.path.exists(input_file):
-                arch_inputs = pd.read_excel(input_file, sheet_name="arch")
-            else:
-                print(
-                    "Archetypes input file "
-                    + input_file
-                    + " does not exist! Please create file for input."
-                )
-
-            if os.path.exists(reg_file):
-                arch_reg = pd.read_excel(reg_file, sheet_name=arch)
-            else:
-                print(
-                    "Regional archetypes input file "
-                    + reg_file
-                    + " does not exist! Please create file for input."
-                )
-
-            # Create map of archetypes based on MESSAGE regions raster
-            arch_map = xr.Dataset(
-                {
-                    "urban": ras.MESSAGE11.copy(deep=True),
-                    "rural": ras.MESSAGE11.copy(deep=True),
-                }
-            )
-
-            for row in arch_reg.itertuples():
-                arch_map["urban"].values[map_reg == row.RegNum] = float(row.urban)
-                arch_map["rural"].values[map_reg == row.RegNum] = float(row.rural)
-
-            arch_map = arch_map.astype(float)
-
-            # TODO: NOT WORKING with integers!!
-            for urt in config.urts:
-                arch_map[urt].values[arch_map[urt] < 0] = np.nan
-
-            # % Write out to netcdf
-
-            arch_map.attrs = {
-                "title": "Archetype IDs by region",
-                "authors": "Edward Byers & Alessio Mastrucci",
-                "date": str(datetime.datetime.now()),
-                "institution": "IIASA Energy Program",
-                "contact": "byers@iiasa.ac.at; mastrucc@iiasa.ac.at",
-                "arch_setting": config.arch_setting,
+        # Create map of archetypes based on MESSAGE regions raster
+        arch_map = xr.Dataset(
+            {
+                "urban": ras.MESSAGE11.copy(deep=True),
+                "rural": ras.MESSAGE11.copy(deep=True),
             }
+        )
 
-            encoding = {var: config.comp for var in arch_map.data_vars}
+        for row in arch_reg.itertuples():
+            arch_map["urban"].values[map_reg == row.RegNum] = float(row.urban)
+            arch_map["rural"].values[map_reg == row.RegNum] = float(row.rural)
 
-            arch_file = "arch_map_" + config.arch_setting + "_" + suff + ".nc"
-            arch_map.to_netcdf(
-                os.path.join(
-                    archetype_path,
-                    arch_file,
-                ),
-                encoding=encoding,
+        arch_map = arch_map.astype(float)
+
+        # TODO: NOT WORKING with integers!!
+        for urt in config.urts:
+            arch_map[urt].values[arch_map[urt] < 0] = np.nan
+
+        # % Write out to netcdf
+
+        arch_map.attrs = {
+            "title": "Archetype IDs by region",
+            "authors": "Edward Byers & Alessio Mastrucci",
+            "date": str(datetime.datetime.now()),
+            "institution": "IIASA Energy Program",
+            "contact": "byers@iiasa.ac.at; mastrucc@iiasa.ac.at",
+            "arch_setting": config.arch_setting,
+        }
+
+        encoding = {var: config.comp for var in arch_map.data_vars}
+
+        arch_file = "arch_map_" + config.arch_setting + "_" + arch + ".nc"
+        arch_map.to_netcdf(
+            os.path.join(
+                archetype_path,
+                arch_file,
+            ),
+            encoding=encoding,
+        )
+
+        print(
+            "-- Saved archetype map to "
+            + os.path.join(
+                archetype_path,
+                arch_file,
             )
-
-            print(
-                "-- Saved archetype map to "
-                + os.path.join(
-                    archetype_path,
-                    arch_file,
-                )
-            )
+        )

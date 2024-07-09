316193e79c66a487cfb2b2f89b888fa465e3c3f1
Measrainsey Meng <measrainsey@pm.me>
2024-07-09 13:23:02 +0200

    Edit to take in arguments

--- message_ix_buildings/chilled/run_preprocess.py
+++ message_ix_buildings/chilled/run_preprocess.py
@@ -1,10 +1,71 @@
-from preprocess.archetypes import (  # type: ignore
-    create_archetype_variables,
+import sys
+from argparse import ArgumentParser
+
+from preprocess.archetypes import (
+    create_archetype_variables,  # type: ignore
     create_archetypes,
 )
 from utils.config import Config  # type: ignore
 
-cfg = Config()
+
+def parse_arguments(arguments):
+    """
+
+    :return:
+    """
+    parser = ArgumentParser(add_help=True)
+
+    parser.add_argument(
+        "-version",
+        "--version",
+        default="ALPS2023",
+        help="Version of inputs to run. Default: ALPS2023.",
+    )
+
+    # Parse arguments
+    parsed_arguments = parser.parse_known_args(args=arguments)[0]
+
+    return parsed_arguments
+
+
+def print_arguments(parsed_arguments):
+    """
+    :param parsed_arguments:
+
+    """
+
+    # Print arguments
+    print("---------- Parsed arguments ------------")
+    print("Selected version: " + parsed_arguments.version)
+
+
+# create climate outputs
+def create_config(parsed_arguments):
+    cfg = Config(
+        vstr=parsed_arguments.version,
+    )
+
+    return cfg
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    parsed_args = parse_arguments(arguments=args)
+
+    # Run the main function
+    print_arguments(parsed_arguments=parsed_args)
+    cfg = create_config(parsed_arguments=parsed_args)
+    create_archetypes(cfg)
+    print("Archetypes created successfully.")
+    create_archetype_variables(cfg)
+    print("Archetype variables created successfully.")
+
+
+if __name__ == "__main__":
+    main()
+
 
 # check to see if "map_reg_MESSAGE_{config.node}.nc" exists
 # if not, create the file
@@ -16,9 +77,6 @@ cfg = Config()
 #     print(f"map_reg_MESSAGE_{cfg.node}.nc already exists. Using existing file.")
 
 # run create_archetypes
-create_archetypes(cfg)
-print("Archetypes created successfully.")
+
 
 # create maps of archetype variables
-create_archetype_variables(cfg)
-print("Archetype variables created successfully.")

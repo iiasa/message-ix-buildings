eb9528434c689f025ff4c99747a688f6c6320ddc
Measrainsey Meng <measrainsey@pm.me>
2024-07-10 06:31:37 +0200

    Remove class for archs in config

--- message_ix_buildings/chilled/utils/config.py
+++ message_ix_buildings/chilled/utils/config.py
@@ -101,7 +101,7 @@ class Config:
     urts = ["urban", "rural"]  # options (mult): "urban", "rural"
 
     # ARCHETYPES
-    archs: list = ["new", "exist"]
+    archs = ["new", "exist"]
 
     # PARAMETERS FOR STEP 2 SCRIPT
     verbose = True

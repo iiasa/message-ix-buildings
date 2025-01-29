72c2ede652a29d3e158bb96feea6c4ae69bc81cf
Measrainsey Meng <measrainsey@pm.me>
2025-01-29 09:07:35 +0100

    Add script to run city-level analysis for given GCM-RCP

---
+++ message_ix_buildings/chilled/cities/run_cities.py
@@ -0,0 +1,107 @@
+import sys
+from argparse import ArgumentParser
+
+import dask
+from dask import delayed
+
+from message_ix_buildings.chilled.cities.util.climate import process_climate_data
+from message_ix_buildings.chilled.util.common import get_logger
+from message_ix_buildings.chilled.util.config import Config  # type: ignore
+
+log = get_logger(__name__)
+
+
+def parse_arguments(arguments):
+    """
+
+    :return:
+    """
+    parser = ArgumentParser(add_help=True)
+
+    parser.add_argument(
+        "-version",
+        "--version",
+        default="ALPS2023",
+        help="Version of inputs to run. Default: ALPS2023.",
+    )
+    parser.add_argument(
+        "-gcm",
+        "--gcm",
+        default="MRI-ESM2-0",
+        help="GCM to run. Options: GFDL-ESM4, IPSL-CM6A-LR, MPI-ESM1-2-HR, MRI-ESM2-0, \
+            UKESM1-0-LL. Default: GFDL-ESM4.",
+    )
+    parser.add_argument(
+        "-rcp",
+        "--rcp",
+        default="baseline",
+        help="RCP to run. Options: ssp126, ssp370, ssp585, baseline. \
+            Default: baseline.",
+    )
+    parser.add_argument(
+        "-lcz",
+        "--lcz",
+        default=True,
+        help="Run with local climate zones. \
+            Default: True.",
+    )
+
+    # Parse arguments
+    parsed_arguments = parser.parse_known_args(args=arguments)[0]
+
+    return parsed_arguments
+
+
+def print_arguments(parsed_arguments):
+    """
+    :param parsed_arguments:
+
+    """
+
+    # Print arguments
+    log.info(
+        "\n"
+        + "---------- Parsed arguments ------------"
+        + "\n"
+        + "Selected version: "
+        + parsed_arguments.version
+        + "\n"
+        + "Selected GCM: "
+        + parsed_arguments.gcm
+        + "\n"
+        + "Selected RCP scenario: "
+        + parsed_arguments.rcp
+        + "\n"
+        + "Run with local climate zones: "
+        + str(parsed_arguments.lcz)
+    )
+
+
+# create climate outputs
+def create_config(parsed_arguments):
+    cfg = Config(
+        vstr=parsed_arguments.version,
+        gcm=parsed_arguments.gcm,
+        rcp=parsed_arguments.rcp,
+    )
+
+    return cfg
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+
+    parsed_args = parse_arguments(arguments=args)
+    climate_zones = parsed_args.lcz
+    cfg = create_config(parsed_arguments=parsed_args)
+
+    # Run the core functions
+    print_arguments(parsed_arguments=parsed_args)
+    log.info("Running core functions...")
+    task = delayed(process_climate_data)(cfg, climate_zones)
+    dask.compute(*task, scheduler="processes")
+
+
+if __name__ == "__main__":
+    main()
